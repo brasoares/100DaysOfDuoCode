d6256ab262039875341e317523f5634244edc52d
Henoc Freire <henocsoares@hotmail.com.br>
2025-04-24 23:27:15 -0300

    Create BandNameGenerator.java
    Creating the first Java program for this Duo Challenge 🤩

---
+++ Day001/BandNameGenerator.java
@@ -0,0 +1,14 @@
+public class BandNameGenerator { // Class declaration
+  public static void main(String[] args) { // Main method
+    // Creates a Scanner object to read input
+    Scanner scanner = new Scanner(System.in);
+
+    // Variable declarations
+    String bornWhere;
+    String petName;
+
+    System.out.println("In which city were you born?");
+    bornWhere = scanner.nextLine();
+    
+  }
+}
