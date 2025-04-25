161c8fd0107c700cdde2fc5eae425e3ba0775225
Henoc Freire <henocsoares@hotmail.com.br>
2025-04-24 23:41:53 -0300

    Add files via upload
    Python file! 🤩

---
+++ Day001/band_name_generator.py
@@ -0,0 +1,11 @@
+# DAY ONE on 2025.04.24
+
+# This program will gather some user data and generate a band name for the person
+
+print("Welcome to the Band Name Generator")
+
+city = input("In which city were you born?\n")
+
+pets_name = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city} {pets_name}")
